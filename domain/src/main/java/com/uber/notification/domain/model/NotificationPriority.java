de32b04f6b7ff3e20578391164b029522f24d8e6
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-09-13 22:18:10 +0530

    Commit 027: domain/src/main/java/com/uber/notification/domain/model/NotificationPriority.java

---
+++ domain/src/main/java/com/uber/notification/domain/model/NotificationPriority.java
@@ -0,0 +1,8 @@
+package com.uber.notification.domain.model;
+
+/** Delivery priority: drives provider timeout choice and inbox ordering. */
+public enum NotificationPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
