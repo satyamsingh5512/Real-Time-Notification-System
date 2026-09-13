5624045d5234b64870e89156ab7f1f50957ac57e
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-09-13 22:18:10 +0530

    Commit 066: infrastructure/src/main/java/com/uber/notification/infrastructure/config/KafkaConfig.java

--- infrastructure/src/main/java/com/uber/notification/infrastructure/config/KafkaConfig.java
+++ infrastructure/src/main/java/com/uber/notification/infrastructure/config/KafkaConfig.java
@@ -24,7 +24,7 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapServers;
 
     @Bean
