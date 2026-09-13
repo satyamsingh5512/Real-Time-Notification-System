ea2a651ef56a4e5a5c6df6426379cbd780db17f3
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-09-13 22:18:09 +0530

    Commit 005: api/src/main/java/com/uber/notification/api/NotificationPlatformApplication.java

--- api/src/main/java/com/uber/notification/api/NotificationPlatformApplication.java
+++ api/src/main/java/com/uber/notification/api/NotificationPlatformApplication.java
@@ -2,10 +2,14 @@ package com.uber.notification.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.uber.notification")
+@EnableJpaRepositories(basePackages = "com.uber.notification.infrastructure.persistence.jpa")
+@EntityScan(basePackages = "com.uber.notification.infrastructure.persistence.entity")
 public class NotificationPlatformApplication {
 
     public static void main(String[] args) {
